cdae8cb1a958c2dd56ce9903f34cb0a4af8e4829
DavidsDvm <davids10.dvm@gmail.com>
2021-05-10 18:02:28 -0500

    Creation of an excercise on python about the physics

---
+++ Console/Python/test2.py
@@ -0,0 +1,11 @@
+"""
+calculate the velocity of an object
+"""
+
+distancy = int(input("Ingrese la distancia del vehiculo (en metros): "))
+
+time = float(input("Ingrese el tiempo del vehiculo (en segundos): "))
+
+velocity = distancy / time
+
+print ("La velocidad es :", velocity)
