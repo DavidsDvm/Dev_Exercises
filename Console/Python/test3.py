ec9df50c9e143939b6466d0d2420679b86f22497
DavidsDvm <davids10.dvm@gmail.com>
2021-05-10 18:03:07 -0500

    Creation of an excercise about an employ that want to know his salary

---
+++ Console/Python/test3.py
@@ -0,0 +1,10 @@
+
+
+name = input ('Digita tu nombre: ')
+id = input ('Digite su numero identificacion: ')
+workedHours = int(input ('Digite las horas trabajadas: '))
+hourValue = int(input ('Digite el valor de las horas: '))
+
+total = workedHours * hourValue
+
+print ("Lo que gano el trabajador por las horas realizadas fue: ", total)
