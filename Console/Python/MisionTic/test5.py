ac236f3f6d7f6206532a5ba2ecc320acd5348ffb
DavidsDvm <davids10.dvm@gmail.com>
2021-05-13 08:50:15 -0500

    Creation of a new excersice on python

---
+++ Console/Python/MisionTic/test5.py
@@ -0,0 +1,12 @@
+"""
+Programa que busque el area y el perimetro de un circulo
+"""
+
+print ("~=======================================~")
+radio = int(input("Ingrese el radio del circulo"))
+print ("~=======================================~")
+area = 3.1416 * (radio ** 2)
+perimetro = (radio * 2) * 3.1416
+
+print ("El area de su circulo es igual a",area,"el perimetro de su ciruculo es",perimetro)
+print ("~=======================================~")
