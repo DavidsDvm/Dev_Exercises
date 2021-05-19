47a2874b4e99fdab9eec05f655a3d37b2ce5e6e7
DavidsDvm <davids10.dvm@gmail.com>
2021-05-18 20:48:41 -0500

    Creation of a new python exercise about functions

---
+++ Console/Python/MisionTic/Excercises/test12.py
@@ -0,0 +1,25 @@
+"""
+    Programa para explicar ejemplos sobre funciones:
+    1. Ejemplo_1: devuelve un <<None>>
+    2. Ejemplo_2: devuelve el resultado de una suma
+    3. Ejemplo_3: devuelve
+"""
+
+__autor__ = "DavidsDvm"
+__version__ = 0.1
+
+def funcion_ejemplo_1():
+    """
+        Devuelve un valor <<None>>"
+    """
+    pass    #representa el bloque de instrucciones que no hace nada
+
+print(funcion_ejemplo_1())
+
+def funcion_ejemplo_2(a, b):
+    """
+        Retorna el resultado de una suma
+    """
+    return a + b
+
+print(funcion_ejemplo_2(2, 2))
