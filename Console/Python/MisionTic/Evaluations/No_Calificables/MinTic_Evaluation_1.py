8ea506a6fd26179732dc6babc7ba06d3e25d57de
DavidsDvm <davids10.dvm@gmail.com>
2021-05-31 22:19:07 -0500

    Creation of the first evaluation no calificable of the Mintic 2022 (folder moved)

---
+++ Console/Python/MisionTic/Evaluations/No_Calificables/MinTic_Evaluation_1.py
@@ -0,0 +1,37 @@
+#=_Describcion del problema_=
+"""
+    Enunciado:
+
+Realiza un programa que cumpla con el siguiente algoritmo, utilizando siempre que sea posible,
+operadores en asignación: Guarda en una variable numero_magico el valor 12345679 (sin el 8)
+Asignar a otra variable numero_usuario. Hay que especificar que sea entre 1 y 9 
+(asegúrate que es un número) Multiplica el numero_usuario por 9 en sí mismo Multiplica
+el numero_magico por el numero_usuario en sí mismo Finalmente muestra el valor final 
+del numero_magico por pantalla
+
+    Dificultad:
+
+Media
+
+    Instrucciones:
+
+Para la solución de este problema, se requiere que el usuario ingrese un número entero
+y el sistema realice el cálculo respectivo para hallar la multiplicación.
+Para esto se debe usar la siguiente expresión. (multiplicación = numero_usuario X numero_magico).
+Por último, realizar la impresión por pantalla de la multiplicación
+"""
+#=_| Elaborado por : David Vargas Monroy | Grupo: 24 | Operaciones matemáticas con números en Python |_=
+
+numero_magico = 12345679
+def excNum(numero_magico):
+    numero_usuario = int(input("Ingresa un numero del 1 al 9: "))
+    if (numero_usuario >= 1 and numero_usuario<=9):
+        numero_usuario *= 9
+        numero_magico *= numero_usuario
+        print(numero_magico)
+    else:
+        print ("No se admiten numeros que no esten dentro del rango del 1 al 9 !!!")
+        print ("Vuelve a intentarlo")
+        print ("~====================================================================~")
+        excNum(numero_magico)
+excNum(numero_magico)
