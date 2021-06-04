4fc25fd8ce0906d39000034a2e227aff1d9c96a0
DavidsDvm <davids10.dvm@gmail.com>
2021-06-04 18:43:42 -0500

    Excersise of python for loop summing the numbers on the 100 range

---
+++ Console/Python/MisionTic/Excercises/test20.py
@@ -0,0 +1,10 @@
+"""
+    La suma de los primeros 100 numeros naturales
+"""
+
+num = 0
+
+for i in range(100):
+    num += i
+    print(num)
+print("La suma de todos los numeros naturales hasta 100 es: {}".format(num))
