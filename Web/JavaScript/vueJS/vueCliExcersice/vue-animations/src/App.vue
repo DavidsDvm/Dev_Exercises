527d912a7c38eec719a35827de2200d1fbf5360e
DavidsDvm <davids10.dvm@gmail.com>
2021-09-18 23:17:03 -0500

    App component of vue

---
+++ Web/JavaScript/vueJS/vueCliExcersice/vue-animations/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <transition name="view" appear enter-active-class="animate__animated animate__backInDown" leave-active-class="animate__animated animate__bounceOut">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
