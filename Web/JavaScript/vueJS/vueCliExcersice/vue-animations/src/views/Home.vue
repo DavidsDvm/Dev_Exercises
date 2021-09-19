4cdf2eb492442c2abdee5acc759da90c01669b81
DavidsDvm <davids10.dvm@gmail.com>
2021-09-18 23:17:50 -0500

    home vue file with some images with animations

---
+++ Web/JavaScript/vueJS/vueCliExcersice/vue-animations/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <button @click="animated = !animated">Animation</button>
+     <section>
+      <transition name="animate" enter-active-class="animate__animated animate__backInDown" leave-active-class="animate__animated animate__bounceOut">
+        <img v-if="animated" alt="Vue logo" src="../assets/logo.png">
+      </transition><br><hr>
+      <div class="imagen2" @mouseover="hoverOver" @mouseout="hoverOut">
+        <img :class="classes" alt="Vue logo" src="../assets/logo.png">
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      animated: false,
+      classes: []
+    };
+  },
+  methods: {
+    hoverOver() {
+      this.classes = ['animate__animated', 'animate__backInDown'];
+    },
+    hoverOut() {
+      setTimeout(() => {this.classes = []}, 1000);
+    }
+  }
+}
+</script>
+
+<style>
+.imagen2 {
+  margin: 0 auto 0 auto;
+  width: 200px;
+  height: 200px;
+}
+</style>
