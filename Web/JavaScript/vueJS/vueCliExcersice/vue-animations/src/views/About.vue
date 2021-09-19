e8b952587e0fbade006321985e371579ec3f67f2
DavidsDvm <davids10.dvm@gmail.com>
2021-09-18 23:18:13 -0500

    about vue file with some test of vue

---
+++ Web/JavaScript/vueJS/vueCliExcersice/vue-animations/src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>{{grettig}}</h1>
+    <input @keypress.enter="salute" v-model="grettig">
+  </div>
+</template>
+
+<script>
+
+export default({
+  data () {
+    return {
+      grettig: "asda"
+    };
+  },
+  methods: {
+    salute() {
+      console.log(this.grettig);
+    }
+  }
+})
+</script>
